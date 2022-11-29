47a9623d84039b682a858d38acc3bb6ecdfe8cdc
alaxhenry <alaxhenry0121@gmail.com>
2022-11-29 17:39:37 +0900

    Initial Commit

---
+++ DashboardAppUIForMacOS/DashboardAppUIForMacOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  DashboardAppUIForMacOSApp.swift
+//  DashboardAppUIForMacOS
+//
+//  Created by Seungchul Ha on 2022/11/29.
+//
+
+import SwiftUI
+
+@main
+struct DashboardAppUIForMacOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
